df547678bb962e0746068450fa0383d057c101ed
amtrex <amtrex14143@gmail.com>
2020-10-08 21:45:43 -0500

    register al 100%

--- lib/src/pages/register_pages.dart
+++ lib/src/pages/register_pages.dart
@@ -1,24 +1,36 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
-class RegisterPage  extends StatelessWidget {
+class RegisterPage  extends StatefulWidget {
+  @override
+  _RegisterPageState createState() => _RegisterPageState();
+}
+
+class _RegisterPageState extends State<RegisterPage> {
   @override
    Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack(children: <Widget>[registerPage(context),], ),
+      body: Stack(
+        children: <Widget>[
+         backApp(),
+        registerPage(),
+        Center(
+        child: tittles(),
+      ),
+        ], 
+      ),
     );  
   }
 
-    Widget registerPage(context){
+    Widget registerPage(){
     return Container(
-      width : double.infinity,
-      height: double.infinity,
-      color: Color.fromRGBO(255, 255, 255, 1),
       child: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children:[
              SizedBox(
-              height: 250.0,
+              height: 293.0,
             ), 
 
              Container(
@@ -130,5 +142,83 @@ class RegisterPage  extends StatelessWidget {
         ),
       ),
     );
+}
+    Widget backApp(){
+        SingleChildScrollView(
+              child: Column(
+                children: <Widget>[
+                
+                ],
+              ),
+            );
+      final gradiente = Container(
+        width: double.infinity,
+        height: double.infinity,
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: FractionalOffset(0.0,0.6),
+            end: FractionalOffset(0.0,1.0),
+            colors: [
+              Colors.white,
+                Color.fromRGBO(200, 200, 200, 1)
+            ]),
+          ),
+      );
+
+      final orangeBox = Transform.rotate(
+        angle: -pi / 1.0,
+        child: Container(
+          height: 360.0,
+          width: 360.0,
+          decoration: BoxDecoration(
+            color: Color.fromRGBO(251, 85, 23, 1),
+            borderRadius: BorderRadius.only(
+              topRight: Radius.circular(120.0),
+              topLeft: Radius.circular(20.0)
+            )
+          ),
+        ),
+      );
+      
+      
+
+      return Stack(
+        children: <Widget> [
+          gradiente,
+          Positioned(
+            top: -100.0,
+            child: orangeBox
+            )
+        ],
+      );
+    }
+    Widget tittles(){
+return Container(
+  padding: EdgeInsets.all(60.0),
+child: Column(
+  children: <Widget>[
+    image(),
+    desc()
+  ],
+)
+);
+}
+    Widget image(){
+  return Image(
+        image: AssetImage('assets/logoW.png'),
+        height: 120,
+        width: 120,
+        );
+}
+    Widget desc(){
+  return Container(
+  padding: EdgeInsets.all(40.0),
+child: Column(
+  crossAxisAlignment: CrossAxisAlignment.start,
+  children: <Widget> [
+    Text("Ingresar", style: TextStyle(color: Colors.white, fontSize: 30.0, fontWeight: FontWeight.bold),)
+  ],
+)
+);
 }
 }
