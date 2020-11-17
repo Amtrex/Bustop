2b7aba92df96b79861e9b3478e4c2d43ed9ad41d
unknown <leonardox.paez90@gmail.com>
2020-11-17 17:56:24 -0500

    mio

--- lib/src/pages/login_page.dart
+++ lib/src/pages/login_page.dart
@@ -253,57 +253,20 @@ class _LoginPageState extends State<LoginPage> {
   }
 
   Future get_data(user_id) async {
-    var roles;
     try {
-      var usuario = (await FirebaseFirestore.instance
-          .collection('tblUsuarios')
-          .where("idUsuario == '" + user_id + "'")
-          .get()
-          .then((value) {
-        value.docs.forEach((element) {
-          if (element['idUsuario'] == user_id) {
-            if (element['rol'] == 'administrador' ||
-                element['rol'] == 'control' ||
-                element['rol'] == 'conductor' ||
-                element['rol'] == 'usuario') {
-              Navigator.of(context).pop();
-              Navigator.pushNamed(context, 'nav', arguments: element['rol']);
-            } else {
-              Navigator.of(context).pop();
-              return showDialog(
-                  context: context,
-                  builder: (context) {
-                    return AlertDialog(
-                        title: Text('En desarrollo'),
-                        content: Container(
-                          child: Wrap(
-                            children: [
-                              Text('Proximamente podras ingresar a este rol'),
-                              Center(
-                                child: CircularProgressIndicator(
-                                  backgroundColor: Colors.white,
-                                  valueColor: new AlwaysStoppedAnimation<Color>(
-                                      Color.fromRGBO(251, 85, 23, 1)),
-                                ),
-                              ),
-                            ],
-                          ),
-                        ),
-                        actions: <Widget>[
-                          FlatButton(
-                            child: Text(
-                              'aceptar',
-                              style: TextStyle(
-                                  color: Color.fromRGBO(251, 85, 23, 1)),
-                            ),
-                            onPressed: () => Navigator.of(context).pop(),
-                          ),
-                        ]);
-                  });
-            }
-          }
-        });
-      }));
+      await FirebaseFirestore.instance.collection('tblUsuarios').get().then(
+        (value) {
+          value.docs.forEach(
+            (element) {
+              var id = element['idUsuario'];
+              if (id == user_id) {
+                Navigator.of(context).pop();
+                Navigator.pushNamed(context, 'nav', arguments: element['rol']);
+              }
+            },
+          );
+        },
+      );
     } catch (e) {
       print(e);
     }
